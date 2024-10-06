f2ac3c44de3eb5cd2bb25509c3c29b4371d60b29
Miguel Palhas <mpalhas@gmail.com>
2024-10-06 11:29:49 +0100

    enable mouse again

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -4,6 +4,7 @@ return {
     lazy = false,
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {
+      disable_mouse = false,
       restricted_keys = {
         ["w"] = { "n", "x" },
         ["W"] = { "n", "x" },
