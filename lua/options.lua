7a5bc7004502ac6b9cc6cf00d263a17fcbf255d2
Miguel Palhas <mpalhas@gmail.com>
2024-10-29 14:07:36 +0000

    conflicts

--- lua/options.lua
+++ lua/options.lua
@@ -17,5 +17,5 @@ o.fillchars = "eob: ,diff: "
 o.scrolloff = 5
 
 vim.o.guifont = "FiraCode Nerd Font Mono:h12"
-vim.g.neovide_scroll_animation_length = 0.2
-vim.g.neovide_cursor_animation_length = 0.12
+vim.g.neovide_scroll_animation_length = 0.15
+vim.g.neovide_cursor_animation_length = 0.11
