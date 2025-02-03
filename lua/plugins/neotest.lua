182f93b553c71233501b03ace5075ea73a02fc01
Miguel Palhas <mpalhas@gmail.com>
2025-02-03 15:40:35 +0000

    neotest shortcuts

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -18,16 +18,37 @@ return {
       {
         "<leader>nt",
         function()
-          require("neotest").run.run()
-        end,
-        { desc = "Run Neotest" },
-      },
-      {
-        "<leader>nt",
-        function()
           require("neotest").run.run(vim.fn.expand "%")
         end,
         { desc = "Test current file" },
+      },
+      {
+        "<leader>nT",
+        function()
+          require("neotest").run.run()
+        end,
+        { desc = "Run test suite" },
+      },
+      {
+        "<leader>ns",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        { desc = "Toggle neotest summary" },
+      },
+      {
+        "<leader>nw",
+        function()
+          require("neotest").watch.watch(vim.fn.expand "%")
+        end,
+        { desc = "Watch current file" },
+      },
+      {
+        "<leader>nW",
+        function()
+          require("neotest").watch.watch()
+        end,
+        { desc = "Watch test suite" },
       },
     },
   },
