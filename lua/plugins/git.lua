671b8fa16c57bdd19766ae9a08c5e5bd8ff052f1
Miguel Palhas <mpalhas@gmail.com>
2026-02-21 14:51:03 +0000

    wip

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -32,18 +32,4 @@ return {
   },
 
   { "akinsho/git-conflict.nvim", event = "VeryLazy", config = true },
-
-  {
-    "georgeguimaraes/review.nvim",
-    dependencies = {
-      "esmuellert/codediff.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-    cmd = { "Review" },
-    keys = {
-      { "<leader>r", "<cmd>Review<cr>", desc = "Review" },
-      { "<leader>R", "<cmd>Review commits<cr>", desc = "Review commits" },
-    },
-    opts = {},
-  },
 }
