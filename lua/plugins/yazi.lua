a6caf577a18453fb5d4f627655d582b2553a9920
Miguel Palhas <mpalhas@gmail.com>
2026-03-03 11:07:43 +0000

    replace mini.files in favor of yazi.nvim

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>y", "<cmd>Yazi<cr>", desc = "Open yazi at current file" },
+      { "<leader>Y", "<cmd>Yazi cwd<cr>", desc = "Open yazi in cwd" },
+    },
+    opts = {
+      open_for_directories = false,
+    },
+  },
+}
